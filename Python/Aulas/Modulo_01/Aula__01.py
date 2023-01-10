68557681de7a7067810e56008b3a64a28f009457
Inacio Grandini Ulbinski <inaciogranbinski@gmail.com>
2023-01-10 10:56:38 -0300

    Update

---
+++ Python/Aulas/Modulo_01/Aula__01.py
@@ -0,0 +1,19 @@
+idade = 30
+print(idade)
+
+idade = 27
+print(idade)
+
+nome = "Idade"
+
+preco = 1000
+tipo_preco = type(preco)
+
+print(preco)
+print(tipo_preco)
+
+juros = 0.5
+tipo_juros = type(juros)
+
+print(juros)
+print(tipo_juros)
