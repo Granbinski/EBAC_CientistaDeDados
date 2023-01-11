cf2201dc6e64915e4de2d7c3b2a1dee5325cc47d
Inacio Grandini Ulbinski <inaciogranbinski@gmail.com>
2023-01-11 08:35:29 -0300

    update

--- Python/Aulas/Modulo_01/Aula_02.py
+++ Python/Aulas/Modulo_01/Aula_02.py
@@ -2,3 +2,39 @@
 print(type(37))
 print(type(10.1))
 print(type(1 + 2j))
+
+# Operações
+
+qtd_items_carrinho_compra = 0
+qtd_items_carrinho_compra = qtd_items_carrinho_compra + 1
+print(qtd_items_carrinho_compra)
+qtd_items_carrinho_compra = qtd_items_carrinho_compra + 1
+print(qtd_items_carrinho_compra)
+
+
+qtd_items_carrinho_compra = 0
+qtd_items_carrinho_compra += 1
+print(qtd_items_carrinho_compra)
+qtd_items_carrinho_compra += 1
+print(qtd_items_carrinho_compra)
+
+preco = 47
+quantidade = 0.250
+total_a_pagar = quantidade * preco
+print(total_a_pagar)
+
+a = 3
+b = 2
+c = a / b
+print(c)
+print(type(c))
+d = a // b
+print(d)
+print(type(d))
+
+
+# Conversao
+
+print(int(3.9))
+print(float(10))
+print(complex(1))
