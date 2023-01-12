e6ecf355d5edcc1b858be0d907f8a5c78c40b2c8
Inacio Grandini Ulbinski <inaciogranbinski@gmail.com>
2023-01-12 10:22:25 -0300

    update

--- Python/Aulas/Modulo_01/Aula_02.py
+++ Python/Aulas/Modulo_01/Aula_02.py
@@ -38,3 +38,29 @@ print(type(d))
 print(int(3.9))
 print(float(10))
 print(complex(1))
+
+
+# TKT Medio
+
+svv_19 = 153.98
+sqv_19 = 3
+
+tkt_19 = svv_19 / sqv_19
+print(tkt_19)
+
+
+svv_20 = 337.01
+sqv_20 = 7
+
+tkt_20 = svv_20 / sqv_20
+print(tkt_20)
+
+
+svv_23 = 295.33
+sqv_23 = 5
+
+tkt_23 = svv_23 / sqv_23
+print(tkt_23)
+
+tkt = (tkt_23 + tkt_20 + tkt_19) / 3
+print(tkt)
---
+++ Python/Aulas/Modulo_01/Aula_03.py
@@ -0,0 +1,23 @@
+# STrings
+
+# sua empresa
+lat = '-22.005320'
+lon = '47.891040'
+
+# startup adiquirida
+latlon = '-22.005320;-47.891040'
+
+nome_aula = "Aula 04, Modulo 01, Strings"
+print(nome_aula)
+print(type(nome_aula))
+
+string_vazia = ""
+print(string_vazia)
+print(type(string_vazia))
+
+# concatenacao
+
+nome = 'Inacio'
+sobrenome = "Granbinski"
+apresentacao = 'Ola, meu nome e ' + nome + " " + sobrenome + "."
+print(apresentacao)
