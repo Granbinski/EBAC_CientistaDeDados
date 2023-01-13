6c4b938fcde9991d7aa79446e6f1f47367e7661d
Inacio Grandini Ulbinski <inaciogranbinski@gmail.com>
2023-01-12 21:43:50 -0300

    update

---
+++ Python/Aulas/Modulo_01/Aula_04.py
@@ -0,0 +1,20 @@
+# Booleanos
+
+usuario = 'inacio.granbinski'
+senha = 'inacio123'
+
+usuario_cadastro = 'andre.perez'
+senha_cadastro = 'inacio321'
+
+verdadeiro = True
+print(verdadeiro)
+
+falso = False
+print(falso)
+
+
+saldo_em_conta = 200
+valor_do_saque = 100
+
+pode_executar_saque = valor_do_saque <= saldo_em_conta
+print(pode_executar_saque)
