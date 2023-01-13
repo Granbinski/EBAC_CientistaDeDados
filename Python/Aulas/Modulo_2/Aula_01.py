2debab0c202a61e63b7e8b386553ec5fa16945a0
Inacio Grandini Ulbinski <inaciogranbinski@gmail.com>
2023-01-13 15:56:12 -0300

    update

--- Python/Aulas/Modulo_2/Aula_01.py
+++ Python/Aulas/Modulo_2/Aula_01.py
@@ -105,7 +105,7 @@ dia_11_saldo_inicial = 1000
 
 
 dia_11_transacoes = []
-dia_11_transacoes.append(243)
+dia_11_transacoes.append(243.0)
 dia_11_transacoes.append(-798.58)
 dia_11_transacoes.append(427.12)
 dia_11_transacoes.append(-10.91)
