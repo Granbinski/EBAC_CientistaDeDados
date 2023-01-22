7ee8ce587ed2dcdc3f19a43d30e89d484af68eda
Inacio Grandini Ulbinski <inaciogranbinski@gmail.com>
2023-01-21 22:18:55 -0400

    update

---
+++ Python/Aulas/Modulo_08/Aula_01.py
@@ -0,0 +1,14 @@
+# pessoa = {'nome': 'Andre Perez', 'idade': 19}
+#    if pessoa['idade'] > 18:
+#        print(True)"""
+
+# print(1 + 'Andre')
+
+i = 0
+while True:
+    # bloco de código
+    i = 1
+    i + 1
+    if i > 10:
+        break
+print(i)
